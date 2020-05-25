d41498a9765527ae1fff65ea0c5d1b6b791b588d
Shejza <shejzaberisha@gmail.com>
2020-05-25 02:56:07 +0200

    Updates

---
+++ precache-manifest.60f3b8d6fd606435ac3d6398f18c0249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d079a05f645bced384c752e43b27ebe",
+    "url": "/pizza-react/index.html"
+  },
+  {
+    "revision": "3f1688ca6f4ac66a4cf0",
+    "url": "/pizza-react/static/js/2.c4702dde.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-react/static/js/2.c4702dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c11ff4fd51b88c25ced",
+    "url": "/pizza-react/static/js/main.52336ef3.chunk.js"
+  },
+  {
+    "revision": "3b23b8861485a88f7112",
+    "url": "/pizza-react/static/js/runtime-main.76046d77.js"
+  }
+]);
